6f577fa72b2d92091e1316c091750b5db9c6e5c6
ismail19659 <ismailnadeem18@gmail.com>
2024-11-12 08:55:28 +0400

    Add files via upload

---
+++ simplesums.py
@@ -0,0 +1,11 @@
+# Step 1: Declare a variable and initialize it with the integer value 8
+first_number = 8
+
+# Step 2: Declare a second variable and initialize it with the integer value 10
+second_number = 10
+
+# Step 3: Declare a third variable that stores the sum of the first two numbers
+sum_of_numbers = first_number + second_number
+
+# Step 4: Print the value of the sum to the console
+print(8+10)
